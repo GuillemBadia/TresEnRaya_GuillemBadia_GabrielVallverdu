0b8f1b510e639742e50ea95f934c1bdca85a7afc
GabrielVallverdu <gabriel.vallverdu@estudiant.enti.cat>
2026-02-27 13:22:42 +0100

    Implement client turn handling in client.sh

--- client.sh
+++ client.sh
@@ -22,8 +22,25 @@ while true; do
   # CLIENT_WIN
   # MOVE_CLIENT
   # ...
-
+  
   # == TORN CLIENT ==
+
+  # 4.4 S'envia al client que comença el seu torn
+  echo "Te toca"
+  # 4.5 Es llegeix el moviment del client
+  read -p "Posició del client (1-9): " pos
+  # 4.6 S'actualitza el moviment al tauler
+  board_index=$((pos - 1))
+  BOARD[$board_index]="0"
+  
+  # 4.7 Es comprova si s'ha guanyat (result="WIN" o result="NONE")
+  result=$(check_win)
+  if [[ "$result" == "WIN" ]]; then
+    echo "CLIENT_WIN" | nc -q 0 $SERVER_IP $PORT
+	break
+fi
+  # 4.8 Es printa el tauler
+  print_board
   # TODO: pregunta posició i s'envia al servidor
   echo "MOVE $pos" | nc -q 0 $SERVER_IP $PORT
 
