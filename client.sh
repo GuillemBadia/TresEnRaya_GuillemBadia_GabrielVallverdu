b4eca7323cf142c4e46219596c9989559b4f2387
Guillem <guillem.badia@estudiant.enti.cat>
2026-03-08 18:21:33 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -22,27 +22,33 @@ while true; do
   # CLIENT_WIN
   # MOVE_CLIENT
   # ...
-  
+  if [[ "$response" == "SERVER_WIN" ]]; then
+    echo "SERVER_WIN" | nc -q 0 $SERVER_IP $PORT
+	break
+	fi
   # == TORN CLIENT ==
 
-  # 4.4 S'envia al client que comença el seu torn
-  echo "Te toca"
-  # 4.5 Es llegeix el moviment del client
-  read -p "Posició del client (1-9): " pos
-  # 4.6 S'actualitza el moviment al tauler
-  board_index=$((pos - 1))
-  BOARD[$board_index]="0"
-  
-  # 4.7 Es comprova si s'ha guanyat (result="WIN" o result="NONE")
-  result=$(check_win)
-  if [[ "$result" == "WIN" ]]; then
-    echo "CLIENT_WIN" | nc -q 0 $SERVER_IP $PORT
-	break
-fi
-  # 4.8 Es printa el tauler
-  print_board
-  # TODO: pregunta posició i s'envia al servidor
-  echo "MOVE $pos" | nc -q 0 $SERVER_IP $PORT
+	if [[ "$response" == "MOVE_CLIENT" ]]; then
+      # 4.4 S'envia al client que comença el seu torn
+	  echo "Te toca"
+	  # 4.5 Es llegeix el moviment del client
+	  read -p "Posició del client (1-9): " pos
+	  # 4.6 S'actualitza el moviment al tauler
+	  board_index=$((pos - 1))
+	  BOARD[$board_index]="0"
+	  
+	  # 4.7 Es comprova si s'ha guanyat (result="WIN" o result="NONE")
+	  result=$(check_win)
+	  if [[ "$result" == "WIN" ]]; then
+	    echo "CLIENT_WIN" | nc -q 0 $SERVER_IP $PORT
+		break
+	  fi
+	  # 4.8 Es printa el tauler
+	  print_board
+	  # pregunta posició i s'envia al servidor
+	  echo "MOVE $pos" | nc -q 0 $SERVER_IP $PORT
+	fi
+
 
 done
 
